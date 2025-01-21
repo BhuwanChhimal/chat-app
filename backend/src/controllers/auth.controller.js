4e59db7ba31212d4ff3f6352277b9608bc062e01
BhuwanChhimal <bhuwanchhimal@gmail.com>
2025-01-21 21:17:10 +0545

    login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,10 +49,47 @@ export const signup = async (req,res) =>{
     }
 }
 
-export const login = (req,res) =>{
-    
+export const login = async (req,res) =>{
+    const { email,password} = req.body
+    try {
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message:"Invalid Credentials"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid Credentials"})
+
+        }
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+
+        })
+    } catch (error) {
+        console.log("error in login controller", error.message)
+        return res.status(500).json({message:"Internal Server Error"})
+
+    }
 }
 
 export const logout = (req,res) =>{
+    try {
+        res.cookie("jwt","",{maxAge:0})
+        return res.status(200).json({message:"Logged Out Successfully"})
+
+    } catch (error) {
+        console.log("error in logout controller", error.message)
+        return res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const updateProfile = async(req,res) =>{
     
 }
