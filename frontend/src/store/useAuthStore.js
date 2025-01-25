0f6fb5c0c7c18e0485880e7e7cd5a6287d6212f5
BhuwanChhimal <bhuwanchhimal@gmail.com>
2025-01-25 11:20:37 +0545

    pages

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,30 @@
+import {create} from "zustand"
+import { axiosInstance } from "../lib/axios.js"
+
+export const useAuthStore = create((set) => ({
+    authUser:null,
+    isSigningUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth:true,
+
+    checkAuth: async() =>{
+        try {
+           const res = await axiosInstance.get("/auth/check") 
+
+           set({authUser: res.data})
+        } catch (error) {
+            console.log("error in check auth",error)
+           set({authUser:null})
+            
+        } finally{
+           set({isCheckingAuth: false})
+
+        }
+    },
+
+    signup: async (data) =>{
+        
+    }
+}))
